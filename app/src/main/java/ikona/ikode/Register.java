1c7bc0743e364d45fe97b26f49b52fbe35fa08fe
Viezara <viezara2015@outlook.com>
2016-07-15 16:28:42 +1200

    Initial commit

---
+++ app/src/main/java/ikona/ikode/Register.java
@@ -0,0 +1,17 @@
+package ikona.ikode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viezara on 14/06/2016.
+ */
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.title_activity_registration);
+        setContentView(R.layout.register);
+    }
+}

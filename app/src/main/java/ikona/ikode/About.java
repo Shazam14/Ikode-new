122938c06a4c44fbdd22380a9cad7b44d89d26f4
Viezara <viezara2015@outlook.com>
2016-07-29 16:42:12 +1200

    about us - with links and codes

--- app/src/main/java/ikona/ikode/About.java
+++ app/src/main/java/ikona/ikode/About.java
@@ -1,7 +1,11 @@
 package ikona.ikode;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.devspark.robototextview.widget.RobotoButton;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        RobotoButton aboutappbutton = (RobotoButton)findViewById(R.id.AboutApp);
+        aboutappbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent iaboutapp = new Intent(About.this, AboutUs.class);
+                startActivity(iaboutapp);
+            }
+        });
+
     }
+
+
+
+
 }
